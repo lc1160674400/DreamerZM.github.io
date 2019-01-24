b28b412c9621f4dc7f4843b61cdeaf1291014ff0
zhuming <929649561@qq.com>
2019-01-24 14:19:03 +0800

    删除代码

--- 微信小程序/personal_wx/pages/games/2048/2048.js
+++ 微信小程序/personal_wx/pages/games/2048/2048.js
@@ -33,6 +33,7 @@ Page({
    */
   onLoad: function (options) {
     this.gameInit();
+    this.submitToCloud()
   },
 
   /**
@@ -390,5 +391,29 @@ moveRight:function(board){
         }
       }
     })
+  },
+  /**
+   * 将分数数据提交到微信服务器
+   */
+  submitToCloud:function(){
+    var now = new Date();
+    var year = now.getFullYear(); //得到年份
+    var month = now.getMonth();//得到月份
+    var date = now.getDate();//得到日期
+    var day = now.getDay();//得到周几
+    var hour = now.getHours();//得到小时
+    var minu = now.getMinutes();//得到分钟
+    var sec = now.getSeconds();//得到秒
+    month = month + 1;
+    if (month < 10) month = "0" + month;
+    if (date < 10) date = "0" + date;
+    if (hour < 10) hour = "0" + hour;
+    if (minu < 10) minu = "0" + minu;
+    if (sec < 10) sec = "0" + sec;
+    var time = year + "-" + month + "-" + date + " " + hour + ":" + minu + ":" + sec;
+
+    var score = 100;//跳一跳分数
+    var kvScore = { "key": "score", "value": score };
+    wx.setUserCloudStroage({ "KVDataList": [kvScore] });
   }
 })
