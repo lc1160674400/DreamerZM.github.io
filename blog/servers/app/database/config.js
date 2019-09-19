969d471325009ffd3dec77f6735b053a139e6e24
ZhuMing <929649561@qq.com>
2019-09-19 13:24:03 +0800

    【后端api】

--- blog/servers/app/database/config.js
+++ blog/servers/app/database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mysql: {
-        host: '47.94.87.211127.0.0.1',
+        host: '47.94.87.211',
         user: 'root', //你的数据库账号
         password: 'Qq929649561.', //你的数据库密码
         database: 'blog',//你的数据库名
--- blog/servers/app/routes/questions.js
+++ blog/servers/app/routes/questions.js
@@ -1,16 +1,48 @@
 var Question = require('./question');
 // 获取url参数 依赖于url模块 使用前需要使用
+var mysql = require('mysql');   // 引入mysql
+var dbConfig = require('../database/config');     //引入mysql 配置文件
+var questionSql = require('../database/QuestionSql')    // 引入sql执行命令
 var URL = require('url');
 var express = require('express');
 var router = express.Router();
 
+// 创建一个连接池
+// 使用DBConfig.js的配置信息创建一个MySql链接池
+var pool = mysql.createPool( dbConfig.mysql );
+// 响应一个JSON数据
+var responseJSON = function (res, ret) {
+    if (typeof ret === 'undefined') {
+      res.json({
+          code: '-200',
+          msg: '操作失败'
+      });
+    } else {
+      var dataString = JSON.stringify(ret);
+      var data = JSON.parse(dataString);
+      res.json(data);
+    }
+};
 /* GET question listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource in question');
 });
 
-router.get('/getQuestion', function(req, res, next) {
 
+// 创建一个查询数据库接口
+router.get('/queryAll',(req,res,next)=>{
+  var question = new Question();
+  var params = URL.parse(req.url, true).query;
+  pool.getConnection((err,connection)=>{
+    connection.query(questionSql.queryAll,(err,result)=>{
+      responseJSON(res, result)
+      connection.release();
+    })
+  })
+})
+
+// 监听一个路由接收参数并且返回一堆静态测试数据
+router.get('/getQuestion', function(req, res, next) {
   var question = new Question();
   var params = URL.parse(req.url, true).query;
 
