dde043058293f4b777a44860a451f71e582a0191
ZhuMing <929649561@qq.com>
2019-10-09 13:40:47 +0800

    设置允许跨域

--- blog/servers/app/app.js
+++ blog/servers/app/app.js
@@ -8,11 +8,41 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var questionRouter = require('./routes/questions');
 var app = express();
-
+app.use((req, res, next) => {
+    // 设置是否运行客户端设置 withCredentials
+    // 即在不同域名下发出的请求也可以携带 cookie
+    res.header("Access-Control-Allow-Credentials",true)
+    // 第二个参数表示允许跨域的域名，* 代表所有域名  
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS') // 允许的 http 请求的方法
+    // 允许前台获得的除 Cache-Control、Content-Language、Content-Type、Expires、Last-Modified、Pragma 这几张基本响应头之外的响应头
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    if (req.method == 'OPTIONS') {
+        res.sendStatus(200)
+    } else {
+        next()
+    }
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// 设置允许跨域访问
+app.use((req, res, next) => {
+  // 设置是否运行客户端设置 withCredentials
+  // 即在不同域名下发出的请求也可以携带 cookie
+  res.header("Access-Control-Allow-Credentials",true)
+  // 第二个参数表示允许跨域的域名，* 代表所有域名  
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS') // 允许的 http 请求的方法
+  // 允许前台获得的除 Cache-Control、Content-Language、Content-Type、Expires、Last-Modified、Pragma 这几张基本响应头之外的响应头
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+  if (req.method == 'OPTIONS') {
+      res.sendStatus(200)
+  } else {
+      next()
+  }
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
